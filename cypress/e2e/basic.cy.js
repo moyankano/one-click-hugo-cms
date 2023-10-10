d7fe26f5a0eb398987d27fdf016c6f3fae74267a
Moisés Rodríguez Barrero <moyankano@hotmail.com>
2023-10-11 00:33:24 +0200

    Traducción de literales al español

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -7,22 +7,22 @@ describe('empty spec', () => {
     cy.get('h1').contains('Great coffee with a conscience')
   })
 
-  it('navigates to the product page', () => {
+  /*it('navigates to the product page', () => {
     cy.get('a[href="/products"]').eq(0).click();
     cy.url().should("include", "/products")
     cy.get('h1').contains(/Our Coffee/i)
-  })
+  })*/
 
-  it('navigates to the values page', () => {
+  /*it('navigates to the values page', () => {
     cy.get('a[href="/values"]').eq(0).click();
     cy.url().should("include", "/values")
     cy.get('h1').contains(/Values/i)
-  })
+  })*/
 
   it('navigates to the blog page', () => {
     cy.get('a[href="/post"]').eq(0).click();
     cy.url().should("include", "/post")
-    cy.get('h1').contains(/Latest Stories/i)
+    cy.get('h1').contains(/Últimos Posts/i)
   })
 })
 
